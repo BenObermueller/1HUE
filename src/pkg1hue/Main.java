4fb5a31645c241efbfb93d0b4cf27382a0dca59f
BenObermueller <Obermueller.Ben@sus.htl-grieskirchen.at>
2020-09-26 10:59:46 +0200

    Finished Projekt

--- src/pkg1hue/Main.java
+++ src/pkg1hue/Main.java
@@ -5,6 +5,8 @@
  */
 package pkg1hue;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ben Obermüller
@@ -14,7 +16,15 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Höchste gewuenschte Priemzahl:");
+        EratosthenesPrimeSieve e = new EratosthenesPrimeSieve(Integer.parseInt(s.nextLine()));
+        e.printPrimes();
+        System.out.println("Zahl:");
+        System.out.println(e.isPrime(Integer.parseInt(s.nextLine())));
+        System.out.println("Max Zahl für printNZahlen()");
+        e.printNZahlen(Integer.parseInt(s.nextLine()));
     }
     
 }
